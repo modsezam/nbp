cb403fc20cf8964768c75f748193208db5da3a25
marcin.kobierowski <modsezam@gmail.com>
2019-06-17 23:05:42 +0200

    final v1.3 add error handling

--- src/main/java/com/sda/nbp/Currency.java
+++ src/main/java/com/sda/nbp/Currency.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 public class Currency {
 
+    private String effectiveDate;
     private List<Rates> rates;
 
     public List<Rates> getRates() {
         return rates;
     }
 
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
 }
 
 class Rates {
--- src/main/java/com/sda/nbp/Main.java
+++ src/main/java/com/sda/nbp/Main.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         double currencyUSD = 0;
         double currencyEUR = 0;
@@ -23,19 +23,30 @@ public class Main {
         double currencySaleEUR = 0;
         double currencySaleGBP = 0;
         double currencySaleCHF = 0;
-        double oldPurchaseUSD = 0;
-        double oldPurchaseEUR = 0;
-        double oldPurchaseGBP = 0;
-        double oldPurchaseCHF = 0;
+        double previousPurchaseUSD = 0;
+        double previousPurchaseEUR = 0;
+        double previousPurchaseGBP = 0;
+        double previousPurchaseCHF = 0;
 
         double currencyPLN = 100;
         int numberOfMonths = 1;
+        String patch = "";
 
-        LocalDate date = LocalDate.now().minusMonths(numberOfMonths);
+        LocalDate dateCurrent = LocalDate.now();
+        LocalDate dateOfPreviousExchange = LocalDate.now().minusMonths(numberOfMonths);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        String patch = "http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
-        Currency[] currency = getCurrencies(patch);
+        patch = "http://api.nbp.pl/api/exchangerates/tables/a/?format=json";
+        Currency[] currency;
+        try {
+            currency = getCurrencies(patch);
+        } catch (IOException e) {
+            System.out.println("No connection witch the api NBP: " + e);
+            System.out.println("Check your connection!");
+            return;
+        }
+        String effectiveDate = currency[0].getEffectiveDate();
+        LocalDate dateReadFromJson = LocalDate.parse(effectiveDate);
         for (Rates rate : currency[0].getRates()) {
             if (rate.getCode().equals("USD")) currencyUSD = rate.getMid();
             if (rate.getCode().equals("EUR")) currencyEUR = rate.getMid();
@@ -43,43 +54,65 @@ public class Main {
             if (rate.getCode().equals("CHF")) currencyCHF = rate.getMid();
         }
 
+        System.out.println("Data from: " + dateReadFromJson);
+        if (!dateReadFromJson.equals(dateCurrent)){
+            System.out.println("The date read is different from the current one!");
+        }
+        System.out.println("The current average USD exchange rate is: " + currencyUSD + ", " + currencyPLN + " PLN is worth "
+                + Math.round(currencyPLN * 100 / currencyUSD) / 100.0 + " USD");
+        System.out.println("The current average EUR exchange rate is: " + currencyEUR + ", " + currencyPLN + " PLN is worth "
+                + Math.round(currencyPLN * 100 / currencyEUR) / 100.0 + " EUR");
+        System.out.println("The current average GBP exchange rate is: " + currencyGBP + ", " + currencyPLN + " PLN is worth "
+                + Math.round(currencyPLN * 100 / currencyGBP) / 100.0 + " GBP");
+        System.out.println("The current average CHF exchange rate is: " + currencyCHF + ", " + currencyPLN + " PLN is worth "
+                + Math.round(currencyPLN * 100 / currencyCHF) / 100.0 + " CHF");
+
+
+        System.out.println("\nProfit from exchange " + currencyPLN + " PLN:");
         patch = "http://api.nbp.pl/api/exchangerates/tables/c/?format=json";
-        Currency[] currency2 = getCurrencies(patch);
-        for (Rates rate : currency2[0].getRates()) {
+        Currency[] currentSalesValues;
+        try {
+            currentSalesValues = getCurrencies(patch);
+        } catch (IOException e) {
+            System.out.println("No connection witch the api NBP: " + e);
+            System.out.println("Check your connection!");
+            return;
+        }
+        for (Rates rate : currentSalesValues[0].getRates()) {
             if (rate.getCode().equals("USD")) currencySaleUSD = rate.getAsk();
             if (rate.getCode().equals("EUR")) currencySaleEUR = rate.getAsk();
             if (rate.getCode().equals("GBP")) currencySaleGBP = rate.getAsk();
             if (rate.getCode().equals("CHF")) currencySaleCHF = rate.getAsk();
         }
 
-        patch = "http://api.nbp.pl/api/exchangerates/tables/c/"+ date.format(formatter) +"/?format=json";
-        Currency[] currency3 = getCurrencies(patch);
-        for (Rates rate : currency3[0].getRates()) {
-            if (rate.getCode().equals("USD")) oldPurchaseUSD = rate.getBid();
-            if (rate.getCode().equals("EUR")) oldPurchaseEUR = rate.getBid();
-            if (rate.getCode().equals("GBP")) oldPurchaseGBP = rate.getBid();
-            if (rate.getCode().equals("CHF")) oldPurchaseCHF = rate.getBid();
+        patch = "http://api.nbp.pl/api/exchangerates/tables/c/"+ dateOfPreviousExchange.format(formatter) +"/?format=json";
+        Currency[] previousPurchaseValues = new Currency[0];
+        for (int i = 0; i < 10; i++) {
+            try {
+                previousPurchaseValues = getCurrencies(patch);
+                break;
+            } catch (IOException e) {
+                System.out.println("There is no data in day: " + dateOfPreviousExchange.minusDays(i).format(formatter));
+                System.out.println("System choose day before: " + dateOfPreviousExchange.minusDays(i + 1).format(formatter));
+                patch = "http://api.nbp.pl/api/exchangerates/tables/c/"+ dateOfPreviousExchange.minusDays(i + 1).format(formatter) +"/?format=json";
+            }
         }
-
-        System.out.println("Currency of USD is: " + currencyUSD + ", " + currencyPLN + " PLN is worth "
-                + Math.round(currencyPLN * 100/currencySaleUSD)/100.0 + " USD");
-        System.out.println("Currency of EUR is: " + currencyEUR + ", " + currencyPLN + " PLN is worth "
-                + Math.round(currencyPLN * 100/currencySaleEUR)/100.0 + " EUR");
-        System.out.println("Currency of GBP is: " + currencyGBP + ", " + currencyPLN + " PLN is worth "
-                + Math.round(currencyPLN * 100/currencySaleGBP)/100.0 + " GBP");
-        System.out.println("Currency of CHF is: " + currencyCHF + ", " + currencyPLN + " PLN is worth "
-                + Math.round(currencyPLN * 100/currencySaleCHF)/100.0 + " CHF");
-
-        System.out.println("\nProfit from exchange " + currencyPLN + " PLN:");
+        for (Rates rate : previousPurchaseValues[0].getRates()) {
+            if (rate.getCode().equals("USD")) previousPurchaseUSD = rate.getBid();
+            if (rate.getCode().equals("EUR")) previousPurchaseEUR = rate.getBid();
+            if (rate.getCode().equals("GBP")) previousPurchaseGBP = rate.getBid();
+            if (rate.getCode().equals("CHF")) previousPurchaseCHF = rate.getBid();
+        }
         System.out.println("Profit from buying USD a " + numberOfMonths + " month ago is: " +
-                (Math.round(currencyPLN * 100 / oldPurchaseUSD * currencySaleUSD - currencyPLN * 100) / 100.0) + " PLN");
+                (Math.round(currencyPLN * 100 / previousPurchaseUSD * currencySaleUSD - currencyPLN * 100) / 100.0) + " PLN");
         System.out.println("Profit from buying EUR a " + numberOfMonths + " month ago is: " +
-                (Math.round(currencyPLN * 100 / oldPurchaseEUR * currencySaleEUR - currencyPLN * 100) / 100.0) + " PLN");
+                (Math.round(currencyPLN * 100 / previousPurchaseEUR * currencySaleEUR - currencyPLN * 100) / 100.0) + " PLN");
         System.out.println("Profit from buying GBP a " + numberOfMonths + " month ago is: " +
-                (Math.round(currencyPLN * 100 / oldPurchaseGBP * currencySaleGBP - currencyPLN * 100) / 100.0) + " PLN");
+                (Math.round(currencyPLN * 100 / previousPurchaseGBP * currencySaleGBP - currencyPLN * 100) / 100.0) + " PLN");
         System.out.println("Profit from buying CHF a " + numberOfMonths + " month ago is: " +
-                (Math.round(currencyPLN * 100 / oldPurchaseCHF * currencySaleCHF - currencyPLN * 100) / 100.0) + " PLN");
+                (Math.round(currencyPLN * 100 / previousPurchaseCHF * currencySaleCHF - currencyPLN * 100) / 100.0) + " PLN");
     }
+
 
     private static Currency[] getCurrencies(final String patch) throws IOException {
         URL url = new URL(patch);
@@ -92,6 +125,5 @@ public class Main {
         Gson gson = new GsonBuilder().create();
         return gson.fromJson(json, Currency[].class);
     }
-
 
 }
